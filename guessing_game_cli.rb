455144b3978f1d2e5eb0248082059f08c7507f9b
Li Xie <li_xie1992@hotmail.com>
2019-09-24 13:16:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,30 @@
 # Code your solution here!
+def run_guessing_game
+  number_to_guess = generate_random_number
+  prompt_user
+  number_guessed = capture_input
+  isNumberCorrectlyGuessed = compare_input_with_random_number(number_to_guess, number_guessed)
+  print_statement(isNumberCorrectlyGuessed, number_to_guess)
+end
+
+def generate_random_number
+  rand(5) + 1
+end 
+
+def prompt_user
+  puts("Guess the number!")
+  
+end
+
+def capture_input
+  gets
+end 
+
+def compare_input_with_random_number(number1, number2)
+  return number1 == number2 : true ? false
+  
+end 
+
+def print_statement(isNumberCorrectlyGuessed, number)
+  isNumberCorrectlyGuessed : "You guessed the correct number!" ? "Sorry! The computer guessed #{number}"
+end
