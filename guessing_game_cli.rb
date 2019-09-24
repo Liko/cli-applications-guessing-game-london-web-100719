d4e9c85665960c03657620c947151ffc474f71a4
Li Xie <li_xie1992@hotmail.com>
2019-09-24 13:20:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,9 @@ def run_guessing_game
   number_to_guess = rand(5) + 1 
   puts("Guess the number!")
   number_guessed = gets.chomp
+  number1 == number2 : "You guessed the correct number!" ? "Sorry! The computer guessed #{number}"
+  
+end
   
 # def run_guessing_game
 #   number_to_guess = generate_random_number
